91471397cf3a741d5cd1e262cb58546a8f8fd6fc
katherine Lopez <201913034b@utea.edu.pe>
2022-10-10 00:23:42 -0500

    agregado card e iconos

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,15 @@ class MyApp extends StatelessWidget {
             CircleAvatar(
               radius: 50.0,
               backgroundColor: Colors.white,
-              backgroundImage: NetworkImage(
-                "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg"),
+              backgroundImage: AssetImage("assets/images/persona1.jpeg")
               ),
               Text(
                 "Fiorella de Fatima Guadalupe",
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 29.0,
+                  fontSize: 20.0,
                   fontWeight: FontWeight.w500,
+                  fontFamily: "Kaushan",
                 ),
               ),
               Text(
@@ -38,6 +38,47 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.0,
                  ),
               ),
+              Divider(
+                thickness: 0.70,
+                color: Colors.white38,
+                indent: 120.0,
+                endIndent: 120.0,
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 8.0),
+                elevation: 10.0, //efecto sombra
+                child: ListTile(
+                title: Text("+51 983707845"),
+                subtitle: Text("Telefono"),
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(Icons.check_circle_outline),
+              ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 8.0),
+                elevation: 10.0, //efecto sombra
+                child: ListTile(
+                title: Text("katholom@gmail.com"),
+                subtitle: Text("Correo electronico"),
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(Icons.check_circle_outline),
+              ),
+              ),
+              Row(
+                children: [
+                  Icon(Icons.alarm),
+                  Icon(Icons.alarm),
+                  Icon(Icons.alarm),
+                ],
+              )
           ],
         ),
       ),
